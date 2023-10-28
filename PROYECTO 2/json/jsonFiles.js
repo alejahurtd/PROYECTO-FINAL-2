cbd0d845148e4b4fc4808fa0dcbaeeed9d6dc0f4
gerisastoque <gerisastoque3009@gmail.com>
2023-10-28 17:25:43 -0500

    rectificacion de que se guardaron los cambios

---
+++ PROYECTO 2/json/jsonFiles.js
@@ -0,0 +1,93 @@
+export const playlist = [
+  {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 1.png",
+      "titulo": "Niegame Tres Veces",
+      "artista":"Silvestre Dángon",
+      "album": "Niegame Tres Veces",
+      "año": "2014",
+      "duracion": "4:20",
+      "letra": ""
+  },
+  {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 2.png",
+      "titulo": " Algo Que Se Quede",
+      "artista":"Grupo Niche",
+      "album": "Algo Que Se Quede",
+      "año": "2020",
+      "duracion": "4:20",
+      "letra": ""
+  },
+  {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 3.png",
+      "titulo": "Hello",
+      "artista":"Grupo Niche",
+      "album": "Algo Que Se Queda",
+      "año": "",
+      "duracion": "",
+      "letra": ""
+  },
+  {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 4.png",
+      "titulo": "Salsa",
+      "artista":"Grupo Niche",
+      "album": "Algo Que Se Queda",
+      "año": "",
+      "duracion": "",
+      "letra": ""
+  },
+  {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 5.png",
+      "titulo": "Vallenato",
+      "artista":"Grupo Niche",
+      "album": "Algo Que Se Queda",
+      "año": "",
+      "duracion": "",
+      "letra": ""
+  },
+  {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 6.png",
+      "titulo": "Merengue",
+      "artista":"Grupo Niche",
+      "album": "Algo Que Se Queda",
+      "año": "",
+      "duracion": "",
+      "letra": ""
+  },
+  {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 7.png",
+      "titulo": "",
+      "artista":"Grupo Niche",
+      "album": "Algo Que Se Queda",
+      "año": "",
+      "duracion": "",
+      "letra": ""
+  },
+ {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 8.png",
+      "titulo": "",
+      "artista":"Adele",
+      "album": "Algo Que Se Queda",
+      "año": "",
+      "duracion": "",
+      "letra": ""
+  },
+ {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 9.png",
+      "titulo": "",
+      "artista":"Grupo Niche",
+      "album": "Algo Que Se Queda",
+      "año": "",
+      "duracion": "",
+      "letra": ""
+  },
+  {
+      "imagen": "../Landing/imagenes/Pictures songs/cancion 10.png",
+      "titulo": "",
+      "artista":"Grupo Niche",
+      "album": "Algo Que Se Queda",
+      "año": "",
+      "duracion": "",
+      "letra": ""
+  }
+
+]
--- PROYECTO 2/music/music.js
+++ PROYECTO 2/music/music.js
@@ -6,10 +6,16 @@ async function getText(playlist){
     let myObject= await fetch(playlist);
     lista= await myObject.json();
 
-    for(let i=0; i<lista.playlist.length;i++){
-        render(lista.playlist[i])
-    }
+    // for(let i=0; i<lista.playlist.length;i++){
+    //     renderSong(lista.playlist[i])
+    // }
     
+    lista.playlist.forEach(element => {
+
+        if(element.like){
+            renderSong(element)
+        }
+    });
 }
 const contenedorLista= document.getElementById("contenedorLista")
 contenedorLista.classList.add("songs__Content")
@@ -23,44 +29,55 @@ const songs = document.createElement("div")
 songs.classList.add("songs")
 contenedorLista.appendChild(songs)
 
-const song = document.createElement("div")
-song.classList.add("song")
-songs.appendChild(song)
+function renderSong (element) {
+    const song = document.createElement("div")
+    song.classList.add("song")
+    songs.appendChild(song)
 
-const songContainer = document.createElement("div")
-songContainer.classList.add("songContainer")
-song.appendChild(songContainer)
+    const songContainer = document.createElement("div")
+    songContainer.classList.add("songContainer")
+    song.appendChild(songContainer)
 
+    const aHref = document.createElement('a');
+    // aHref.href = './ruta'
+    songContainer.appendChild(aHref)
 
-
-
-function render(lista){
-    
     const image = document.createElement("img")
-    image.src = lista.imagen
-    image.href = 
-    image.classList.add("songContainer")
-    songContainer.appendChild(image)
+    image.src = element.imagen
+    aHref.appendChild(image)
 
-    const artista = document.createElement("a")
-    artista.innerText = lista.artista
-    artista.classList.add("song__singer")
-    songContainer.appendChild(artista)
+    const songText = document.createElement('div')
+    songText.classList.add("song__Text");
+    songContainer.appendChild(songText)
 
-    
-    const titulo = document.createElement("a")
-    titulo.innerText = lista.titulo
-    titulo.classList.add("song__Title--text")
-    songContainer.appendChild(titulo)
+    const songTitlee = document.createElement('div')
+    songTitlee.classList.add("song__Title");
+    songText.appendChild(songTitlee)
 
-    console.log(lista)
+    const aHref2 = document.createElement('a')
+    // aHref.href = './ruta'
+    songTitlee.innerText = element.titulo
+    songTitlee.appendChild(aHref2)
+
+    const songSinger = document.createElement('div');
+    songSinger.classList.add("song__singer")
+    songSinger.innerText = element.artista
+    songText.appendChild(songSinger)
+
+    const songFavContainer = document.createElement('div')
+    songFavContainer.classList.add('songFav__Container')
+    song.appendChild(songFavContainer)
+
+    const favImg = document.createElement('img')
+    favImg.classList.add('song__Fav')
+    // image.src = './imagen'
+    songFavContainer.appendChild(favImg)
+
+    const time = document.createElement('div')
+    time.classList.add('song__Time')
+    time.innerText = element.duracion
+    songFavContainer.appendChild(time)
 }
-
 
 getText(playlist)
 
-function holi () {
-    console.log("holi")
-}
-
-holi()
